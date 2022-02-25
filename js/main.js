36ce068581bd49d1021bb2652d83237c97e62a1e
shtorm-fakel <ibrohimtalatov2@gmail.com>
2022-02-25 23:16:52 +0300

    creat funtion

--- js/main.js
+++ js/main.js
@@ -1,33 +1,48 @@
-var elForm = document.querySelector (".main__form")
+var elForm = document.querySelector (".main__form");
 
-var elInput = document.querySelector (".form__input")
+var elInput = document.querySelector (".form__input");
 
-var elMan = document.querySelector (".list__man")
+var elMan = document.querySelector (".list__man");
 
-var elBicycle = document.querySelector (".list__bicycle")
+var elBicycle = document.querySelector (".list__bicycle");
 
-var elCar = document.querySelector (".list__car")
+var elCar = document.querySelector (".list__car");
 
-var elPlane = document.querySelector (".list__plane")
+var elPlane = document.querySelector (".list__plane");
 
 elForm.addEventListener( "submit" , function (event){
-    event.preventDefault()
+    event.preventDefault();
 
 var elInputVal = elInput.value;
 
-var Man = 3.6;
+var man = 3.6;
 
-var Bicycle = 20;
+var bicycle = 20;
 
-var Car = 70;
+var car = 70;
 
-var Plane = 800;
+var plane = 800;
 
-elMan.textContent =  Math.round( elInputVal / Man 
-    );
-elBicycle.textContent =  Math.round( elInputVal / Bicycle 
-    );
-elCar.textContent =  Math.round( elInputVal / Car);
-elPlane.textContent =  Math.round( elInputVal / Plane 
-    );
+    function manText (){
+        return elInputVal / man;
+    }
+
+    function bicycleText (){
+        return elInputVal / bicycle;
+    }
+
+    function carText (){
+        return elInputVal / car;
+    }
+
+    function planeText (){
+        return elInputVal / plane;
+    }
+
+    elMan.textContent = Math.round(manText());
+    elBicycle.textContent = Math.round(bicycleText());
+    elCar.textContent = Math.round(carText());
+    elPlane.textContent = Math.round(planeText());
+  
 })
+
